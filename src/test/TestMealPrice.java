f7a27a0501bc23ca2b474cdcb59593ba3dd4b4d4
ysy <1837488472@qq.com>
2022-05-29 01:06:56 +0800

    1-1.34

---
+++ src/test/TestMealPrice.java
@@ -0,0 +1,27 @@
+package test;
+import control.Flight;
+import control.FlightDatabase;
+import control.mealprice;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+public class TestMealPrice {
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(10, new mealprice("Vegetarian").calculatePrice());
+        Assertions.assertEquals(10, new mealprice("Standard and Halal").calculatePrice());
+        Assertions.assertEquals(10, new mealprice("Halal").calculatePrice());
+        Assertions.assertEquals(10, new mealprice("Standard and Vegetarian").calculatePrice());
+        Assertions.assertEquals(50, new mealprice("Gourmet Menu").calculatePrice());
+        Assertions.assertEquals(60, new mealprice("Standard and Gourmet Menu").calculatePrice());
+        Assertions.assertEquals(20, new mealprice("Vegetarian and Halal").calculatePrice());
+        Assertions.assertEquals(70, new mealprice("Vegetarian; Halal and Gourmet Menu").calculatePrice());
+        Assertions.assertEquals(70, new mealprice("Standard; Vegetarian; Halal and Gourmet Menu").calculatePrice());
+        Assertions.assertEquals(0, new mealprice("").calculatePrice());
+        Assertions.assertEquals(60, new mealprice("Vegetarian and Gourmet Menu").calculatePrice());
+
+
+    }
+}
---
+++ src/test/TestSeats.java
@@ -0,0 +1,28 @@
+package test;
+import control.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+public class TestSeats {
+    Seat seat1 = new Seat("2","0");
+    Seats seats = new Seats();
+    Seat seat2 = new Seat("100","100");
+
+    @Test
+    public void testCheckSeatNumber(){
+        Assertions.assertEquals(seat1.toString(), seats.checkSeatNumber("2").toString());
+        Assertions.assertNull(seats.checkSeatNumber(""));
+
+    }
+    @Test
+    public void testAddDelete(){
+        seats.add(seat2);
+        Assertions.assertEquals(seat2.toString(), seats.checkSeatNumber("100").toString());
+
+        seats.delete("100");
+        Assertions.assertNull(seats.checkSeatNumber("100"));
+
+    }
+
+}
